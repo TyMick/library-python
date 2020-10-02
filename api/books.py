6ca8dbca9f39d595c792ef1032bdcdc4f6d39866
Ty Mick <ty@tymick.me>
2020-10-02 14:52:50 -0400

    Add get_one_book

--- api/books.py
+++ api/books.py
@@ -53,22 +53,38 @@ def delete_all_books():
 
 
 def get_one_book(book_id):
-    pass
+    try:
+        db = get_db()
+        c = db.cursor()
+        c.execute("SELECT title, _id FROM book WHERE _id == ?", (book_id,))
+        book = c.fetchone()
+        if book is None:
+            return "No book exists"
+
+        c.execute("SELECT text FROM comment WHERE book_id == ?", (book_id,))
+        book["comments"] = []
+        for row in c.fetchall():
+            book["comments"].append(row["text"])
+
+        return book
+
+    except:
+        return "Database error"
 
 
 def add_comment(book_id):
     comment = request.form.get("comment")
     if not comment:
         return "No comment provided"
-    
+
     try:
         db = get_db()
         c = db.cursor()
         c.execute("INSERT INTO comment(book_id, text) VALUES(?, ?)", (book_id, comment))
         db.commit()
-        
+
         return get_one_book(book_id)
-    
+
     except sqlite3.IntegrityError as e:
         if "FOREIGN KEY constraint failed" in e.args[0]:
             return "No book exists"
